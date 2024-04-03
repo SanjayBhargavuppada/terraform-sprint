84f2424c377a70f5815ab67e72c5c7f2713204b4
SanjayBhargavuppada <uppadasanjaybhargav@gmail.com>
2024-04-03 12:40:21 +0100

    second terraform script to create vars and storage account

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+}
+# Create resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
-  location = "eastus"
-}
-
-# Create a storage account
-resource "azurerm_storage_account" "st" {
-  name                     = "mystorageaccount1202"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# Create storage account
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
